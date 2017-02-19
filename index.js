7902005e20b307b45e25a76b5d9e7247cf8c9d41
Ekhlas Rain <baran.rain1994@gmail.com>
2017-02-19 23:55:17 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+	return true;
+}
+
+function sayHey(){
+	return `Hey!`
+}
+
+function sayHeyFriend(name){
+	return 'Hey, Kristin!'
+}
+
+function favoriteIceCream(chocolate){
+	return `I love ${chocolate}`
+}
+
+function shouting(string){
+	return string.toUpperCase()
+}
+
+function roundDown(number){
+	return Math.floor(number);
+}
